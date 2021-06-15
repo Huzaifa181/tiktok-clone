4bc915c71786a6795e6807cb99b733d379dd518a
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2021-06-16 00:01:55 +0500

    Made the Footer of the videpo front page

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import './VideoFooter.css'
-const VideoFooter = () => {
+import './VideoFooter.css';
+import Ticker from 'react-ticker'
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+const VideoFooter = ({channel, description, song}) => {
     return (
         <div className='videoFooter'>
             <div className="videoFooter__text">
-                <h3>@huzaifa</h3>
-                <p>This is a description</p>
+                <h3>{chaneel}</h3>
+                <p>{description}</p>
+                <div className="videoFooter__ticker">
+                <MusicNoteIcon className='videoFooter__icon'/>
+                <Ticker mode='smooth'>
+                    {index=>{
+                        return (
+                            <p>{song}</p>
+                        )
+                    }}
+                </Ticker>
+                </div>
+                
             </div>
             <img className='videoFooter__record' src="http://static.thenounproject.com/png/934821-200.png" alt="" />
         </div>
